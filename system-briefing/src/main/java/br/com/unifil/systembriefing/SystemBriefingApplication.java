e723165cfacd3f017dab0f12079a442a89c4a319
Endo99 <wellingotn.endo@edu.unifil.br>
2022-05-28 16:18:01 -0300

    ADICONANDO AS COISAS

---
+++ system-briefing/src/main/java/br/com/unifil/systembriefing/SystemBriefingApplication.java
@@ -0,0 +1,13 @@
+package br.com.unifil.systembriefing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemBriefingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemBriefingApplication.class, args);
+	}
+
+}
---
+++ system-briefing/src/test/java/br/com/unifil/systembriefing/SystemBriefingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unifil.systembriefing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemBriefingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
